f330b38d1b0fb6a3ffbbe250684cf847a6125360
coderdal <agircoder@gmail.com>
2023-12-22 12:10:24 +0300

    boilerplate setup

---
+++ app/components/Wheel.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Wheel.module.scss";
+
+const Wheel = () => {
+  return (
+    <section className={styles.wheel}>
+      <h1>Test</h1>
+    </section>
+  );
+};
+
+export default Wheel;
